b7cf14eb8f0f6c1fd99d87592f1919c9fb312e42
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-08-27 05:23:00 +0300

    gameVersions

---
+++ src/tasks/loaders/gameVersions.ts
@@ -0,0 +1,41 @@
+import { S3Client } from '@aws-sdk/client-s3'
+import { I18n, type Snapshot as Snapshot } from '../../utils/utils'
+import { clickhouse } from '@/db'
+import { parseStringPromise } from 'xml2js'
+
+export async function load(root: string, snapshot: Snapshot, i18n: I18n, bucket: S3Client) {
+
+  const pathsFile = Bun.file(`${root}/sources/base/paths.xml`)
+
+  if (!pathsFile.exists()) return
+
+  const paths = await parseStringPromise(await pathsFile.text(), { explicitArray: false, trim: true })
+
+  const modsPath = paths['root']['Paths']['Path'].find((t: any) => t['_'].match(/\.\/mods\/.*/))
+
+  if (!modsPath) return console.warn('No mods path found')
+
+  const modsPathValue = modsPath['_']
+  const modsFolderName = modsPathValue.replace('./mods/', '')
+
+
+  console.log('Inserting game version...')
+  await clickhouse.insert({
+    table: 'WOT.GameVersions',
+    values: [
+      {
+        region: `tmp-${snapshot.realm}`,
+        gameVersionFull: snapshot.full,
+        gameVersion: snapshot.version,
+        gameVersionHash: snapshot.hash,
+        gameVersionComp: snapshot.comparable,
+        datetime: Math.round(new Date().getTime() / 1000),
+
+        modsFolderName: modsFolderName,
+      }
+    ],
+    format: 'JSONEachRow'
+  })
+  console.log(`Game version inserted`)
+
+}
