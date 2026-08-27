0071d19e8fc871a93967eeb5718e0caa7581b8de
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-08-27 04:28:38 +0300

    skills

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -5,6 +5,7 @@ import { load as loadArenas } from './loaders/arenas'
 import { load as loadLootboxes } from './loaders/lootboxes'
 import { load as loadShells } from './loaders/shells'
 import { load as loadComp7 } from './loaders/comp7'
+import { load as loadSkills } from './loaders/skills'
 
 
 const s3Client = new S3Client()
@@ -19,5 +20,6 @@ export async function load(root: string) {
   // try { await loadLootboxes(root, snapshot, i18n, s3Client) } catch (error) { console.error(error) }
   // try { await loadShells(root, snapshot, i18n, s3Client) } catch (error) { console.error(error) }
   // try { await loadComp7(root, snapshot, i18n, s3Client) } catch (error) { console.error(error) }
+  try { await loadSkills(root, snapshot, i18n, s3Client) } catch (error) { console.error(error) }
 
 }
---
+++ src/tasks/loaders/skills.ts
@@ -0,0 +1,39 @@
+import { S3Client } from '@aws-sdk/client-s3'
+import { Glob } from 'bun'
+import { uploader } from '@/utils/assetsUploader'
+import { filenameAndExtension, type I18n, type Snapshot } from '@/utils/utils'
+
+export async function load(root: string, snapshot: Snapshot, i18n: I18n, bucket: S3Client) {
+
+  const upload = uploader(snapshot, bucket)
+
+  const extraLarge = [...new Glob(`${root}/sources/base/res/gui/maps/icons/tankmen/skills/dialogs/*.png`).scanSync()]
+  const big = [...new Glob(`${root}/sources/base/res/gui/maps/icons/tankmen/skills/big/*.png`).scanSync()]
+
+  const intersectedNames = new Set(extraLarge.map(filePath => filenameAndExtension(filePath).nameWithoutExt))
+    .intersection(new Set(big.map(filePath => filenameAndExtension(filePath).nameWithoutExt)))
+
+
+  const uploadings: Promise<void>[] = []
+  for (const filePath of extraLarge) {
+    const { nameWithoutExt: name, ext } = filenameAndExtension(filePath)
+    if (!intersectedNames.has(name)) continue
+
+    const file = Bun.file(filePath)
+    const webpBytes = await file.image().webp({ quality: 80 }).bytes()
+    uploadings.push(upload(`skills/large/${name}.png`, await file.bytes()))
+    uploadings.push(upload(`skills/large/${name}.webp`, webpBytes))
+  }
+
+  for (const filePath of big) {
+    const { nameWithoutExt: name, ext } = filenameAndExtension(filePath)
+    if (!intersectedNames.has(name)) continue
+
+    const file = Bun.file(filePath)
+    const webpBytes = await file.image().webp({ quality: 80 }).bytes()
+    uploadings.push(upload(`skills/medium/${name}.png`, await file.bytes()))
+    uploadings.push(upload(`skills/medium/${name}.webp`, webpBytes))
+  }
+
+  await Promise.all(uploadings)
+}
